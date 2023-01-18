b8cdc1632375143c2d8c43109091624b74676a49
Kluzzio <kluzzio.amp@gmail.com>
2023-01-18 11:21:23 -0800

    Temp push

--- src/main/java/sapphiregaze/amethystitems/mixin/PlayerEntityMixin.java
+++ src/main/java/sapphiregaze/amethystitems/mixin/PlayerEntityMixin.java
@@ -2,9 +2,11 @@ package sapphiregaze.amethystitems.mixin;
 
 import net.minecraft.enchantment.EnchantmentHelper;
 import net.minecraft.entity.player.PlayerEntity;
+import net.minecraft.server.network.ServerPlayerEntity;
 import net.minecraft.sound.SoundEvents;
 import org.spongepowered.asm.mixin.Mixin;
 import org.spongepowered.asm.mixin.injection.At;
+import org.spongepowered.asm.mixin.injection.Inject;
 import org.spongepowered.asm.mixin.injection.ModifyVariable;
 import sapphiregaze.amethystitems.init.EnchantInit;
 import sapphiregaze.amethystitems.util.Utility;
@@ -12,20 +14,19 @@ import sapphiregaze.amethystitems.util.Utility;
 @Mixin(PlayerEntity.class)
 public abstract class PlayerEntityMixin {
 
-    @ModifyVariable(method = "attack", at = @At(value = "STORE"), ordinal = 1)
+    @ModifyVariable(method = "attack", at = @At(value = "STORE", ordinal = 1), ordinal = 0)
     private float addTransposeDamage(float value) {
         PlayerEntity pe = (PlayerEntity) (Object) this;
-        if (!pe.getEntityWorld().isClient()) {
-            int transposeChanceBase = 20;
-            float transposeHeathBase = 1F;
-            int level = EnchantmentHelper.getLevel(EnchantInit.TRANSPOSE, pe.getMainHandStack());
-            if (level > 0 && Utility.percentChance(transposeChanceBase * level)) {
-                value += transposeHeathBase * level;
-                pe.heal(transposeHeathBase * level * pe.getAttackCooldownProgress(0.5f));
-                System.out.println("You've been healed");
-                pe.playSound(SoundEvents.BLOCK_AMETHYST_CLUSTER_HIT, 1.0F, 1.0F);
-            }
+        int transposeChanceBase = 20;
+        float transposeHeathBase = 1F;
+        int level = EnchantmentHelper.getLevel(EnchantInit.TRANSPOSE, pe.getMainHandStack());
+        if (level > 0 && Utility.percentChance(transposeChanceBase * level)) {
+            value += transposeHeathBase * level;
+            pe.heal(transposeHeathBase * level * pe.getAttackCooldownProgress(0.5f));
+            System.out.println("You've been healed");
+            pe.playSound(SoundEvents.BLOCK_AMETHYST_CLUSTER_HIT, 1.0F, 1.0F);
         }
+
         return value;
     }
 }
